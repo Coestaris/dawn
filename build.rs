f15cff80147e5fa9198ae581b57d49cc2944927b
Kurylko Maxim <vk_vm@ukr.net>
2025-08-30 15:38:49 +0300

    Do not pack assets feature

--- build.rs
+++ build.rs
@@ -33,38 +33,40 @@ fn main() {
         res.compile().unwrap();
     }
 
-    let current_dir = std::env::current_dir().unwrap().join("assets");
+    #[cfg(not(feature = "no_assets"))]
+    {
+        let current_dir = std::env::current_dir().unwrap().join("assets");
+        let mut target_dir: PathBuf = std::env::var_os("CARGO_MANIFEST_DIR").unwrap().into();
+        target_dir.push("target");
+        target_dir.push(std::env::var_os("PROFILE").unwrap()); // "debug" or "release"
+        let output = target_dir.join("assets.dac");
 
-    let mut target_dir: PathBuf = std::env::var_os("CARGO_MANIFEST_DIR").unwrap().into();
-    target_dir.push("target");
-    target_dir.push(std::env::var_os("PROFILE").unwrap()); // "debug" or "release"
-    let output = target_dir.join("assets.dac");
+        let file = std::fs::File::create(&output).unwrap();
+        let mut writer = std::io::BufWriter::new(file);
 
-    let file = std::fs::File::create(&output).unwrap();
-    let mut writer = std::io::BufWriter::new(file);
+        // Make compression none in debug mode for faster builds
+        let compression_level = if std::env::var("PROFILE").unwrap() == "release" {
+            CompressionLevel::Default
+        } else {
+            CompressionLevel::None
+        };
 
-    // Make compression none in debug mode for faster builds
-    let compression_level = if std::env::var("PROFILE").unwrap() == "release" {
-        CompressionLevel::Default
-    } else {
-        CompressionLevel::None
-    };
+        write_from_directory(
+            &mut writer,
+            current_dir,
+            WriteConfig {
+                read_mode: ReadMode::Recursive,
+                checksum_algorithm: ChecksumAlgorithm::Blake3,
+                compression_level,
+                cache_dir: cache_dir().unwrap().join("dawn_cache"),
+                author: Some("Coestaris <vk_vm@ukr.net>".to_string()),
+                description: Some("DAWN assets".to_string()),
+                version: Some("0.1.0".to_string()),
+                license: Some("MIT".to_string()),
+            },
+        )
+        .unwrap();
 
-    write_from_directory(
-        &mut writer,
-        current_dir,
-        WriteConfig {
-            read_mode: ReadMode::Recursive,
-            checksum_algorithm: ChecksumAlgorithm::Blake3,
-            compression_level,
-            cache_dir: cache_dir().unwrap().join("dawn_cache"),
-            author: Some("Coestaris <vk_vm@ukr.net>".to_string()),
-            description: Some("DAWN assets".to_string()),
-            version: Some("0.1.0".to_string()),
-            license: Some("MIT".to_string()),
-        },
-    )
-    .unwrap();
-
-    println!("cargo:rerun-if-changed=assets");
+        println!("cargo:rerun-if-changed=assets");
+    }
 }
