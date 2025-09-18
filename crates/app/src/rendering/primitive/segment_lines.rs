13e3d0bdea62a7bdf648cc05d35e58281fcfdc73
Kurylko Maxim <vk_vm@ukr.net>
2025-09-18 16:59:16 +0300

    Sun light gizmo

--- crates/app/src/rendering/primitive/segment_lines.rs
+++ crates/app/src/rendering/primitive/segment_lines.rs
@@ -6,34 +6,18 @@ use dawn_graphics::passes::result::RenderResult;
 use glam::{Mat4, Vec3};
 use std::sync::Arc;
 
-/// Defines the 3D cube primitive as a line loop.
-pub struct Cube3DLines {
+/// Defines the 2-point line primitive as a line loop along the z-axis.
+pub struct Segment3DLines {
     pub vao: VertexArray,
     pub vbo: ArrayBuffer,
     pub ebo: ElementArrayBuffer,
-    pub indices_count: usize,
 }
 
-impl Cube3DLines {
+impl Segment3DLines {
     pub fn new(gl: Arc<glow::Context>) -> Self {
-        let vertex = [
-            // Top face
-            1.0f32, 1.0, 1.0, // 0
-            1.0, 1.0, -1.0, // 1
-            -1.0, 1.0, -1.0, // 2
-            -1.0, 1.0, 1.0, // 3
-            // Bottom face
-            1.0, -1.0, 1.0, // 4
-            1.0, -1.0, -1.0, // 5
-            -1.0, -1.0, -1.0, // 6
-            -1.0, -1.0, 1.0, // 7
-        ];
+        let vertex = [0.0f32, 0.0, 0.0, 0.0, 0.0, 1.0];
+        let indices_edges = [0u16, 1];
 
-        let indices_edges = [
-            0u16, 1, 1, 2, 2, 3, 3, 0, // Top face edges
-            4, 5, 5, 6, 6, 7, 7, 4, // Bottom face edges
-            0, 4, 1, 5, 2, 6, 3, 7, // Side edges
-        ];
         let vao = VertexArray::new(gl.clone(), IRTopology::Lines, IRIndexType::U16).unwrap();
         let mut vbo = ArrayBuffer::new(gl.clone()).unwrap();
         let mut ebo = ElementArrayBuffer::new(gl).unwrap();
@@ -60,29 +44,11 @@ impl Cube3DLines {
         drop(ebo_binding);
         drop(vao_binding);
 
-        Cube3DLines {
-            vao,
-            vbo,
-            ebo,
-            indices_count: indices_edges.len(),
-        }
+        Segment3DLines { vao, vbo, ebo }
     }
 
-    pub fn draw(
-        &self,
-        mut set_model: impl FnMut(Mat4) -> (),
-        min: Vec3,
-        max: Vec3,
-    ) -> RenderResult {
-        // Calculate the translation matrix to transform the 1/1/1 cube to the min/max box
-        let translation = Mat4::from_translation((min + max) / 2.0);
-        let scale = Mat4::from_scale((max - min) / 2.0);
-        // Note: The order of multiplication matters
-        let model = translation * scale;
-
-        set_model(model);
-
+    pub fn draw(&self) -> RenderResult {
         let binding = self.vao.bind();
-        binding.draw_elements(self.indices_count, 0)
+        binding.draw_elements(2, 0)
     }
 }
