f1c4b8081c645be4a7a20e65e512449289860a23
Kurylko Maxim <vk_vm@ukr.net>
2025-09-24 16:52:54 +0300

    WIP

--- crates/app/src/rendering/shaders/ssao_blur.rs
+++ crates/app/src/rendering/shaders/ssao_blur.rs
@@ -4,15 +4,15 @@ use dawn_graphics::gl::raii::shader_program::{Program, UniformLocation};
 pub struct SSAOBlurShader {
     pub asset: TypedAsset<Program>,
 
-    pub ubo_camera_location: u32,
-    pub position_location: UniformLocation,
-    pub ssao_raw_location: UniformLocation,
-    pub rough_occlusion_normal_location: UniformLocation,
+    pub ubo_camera: u32,
+    pub depth: UniformLocation,
+    pub ssao_raw: UniformLocation,
+    pub rough_occlusion_normal: UniformLocation,
 
     pub radius: UniformLocation,
-    pub sigma_spatial_location: UniformLocation,
-    pub sigma_depth_location: UniformLocation,
-    pub sigma_normal_location: UniformLocation,
+    pub sigma_spatial: UniformLocation,
+    pub sigma_depth: UniformLocation,
+    pub sigma_normal: UniformLocation,
     pub ssao_enabled: UniformLocation,
 }
 
@@ -25,18 +25,17 @@ impl SSAOBlurShader {
         Ok(Self {
             asset: clone,
 
-            ubo_camera_location: program.get_uniform_block_location("ubo_camera")?,
+            ubo_camera: program.get_uniform_block_location("ubo_camera")?,
 
-            position_location: program.get_uniform_location("in_position")?,
-            ssao_raw_location: program.get_uniform_location("in_ssao_raw")?,
-            rough_occlusion_normal_location: program
-                .get_uniform_location("in_rough_occlusion_normal")?,
+            depth: program.get_uniform_location("in_depth")?,
+            ssao_raw: program.get_uniform_location("in_ssao_raw")?,
+            rough_occlusion_normal: program.get_uniform_location("in_rough_occlusion_normal")?,
             ssao_enabled: program.get_uniform_location("in_ssao_enabled")?,
 
             radius: program.get_uniform_location("in_radius")?,
-            sigma_spatial_location: program.get_uniform_location("in_sigma_spatial")?,
-            sigma_depth_location: program.get_uniform_location("in_sigma_depth")?,
-            sigma_normal_location: program.get_uniform_location("in_sigma_normal")?,
+            sigma_spatial: program.get_uniform_location("in_sigma_spatial")?,
+            sigma_depth: program.get_uniform_location("in_sigma_depth")?,
+            sigma_normal: program.get_uniform_location("in_sigma_normal")?,
         })
     }
 }
