6c6ada66e7478af09ee38176b7716e4bc2ee1b49
Kurylko Maxim <vk_vm@ukr.net>
2025-10-07 11:28:31 +0300

    Devtools pass

--- crates/app/src/rendering/passes/mod.rs
+++ crates/app/src/rendering/passes/mod.rs
@@ -1,10 +1,8 @@
 #[cfg(feature = "devtools")]
 pub mod devtools_pass;
 pub mod geometry_pass;
-#[cfg(feature = "devtools")]
-pub mod gizmos_pass;
 pub mod lighting_pass;
 pub mod postprocess_pass;
-pub mod ssao_raw;
-pub mod ssao_halfres;
 pub mod ssao_blur;
+pub mod ssao_halfres;
+pub mod ssao_raw;
