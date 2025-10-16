2823466331584a79148248c7a1277f5d5f94910e
Kurylko Maxim <vk_vm@ukr.net>
2025-10-16 13:53:14 +0300

    Small forward_pass refactoring

--- crates/app/src/rendering/passes/forward_pass.rs
+++ crates/app/src/rendering/passes/forward_pass.rs
@@ -7,6 +7,7 @@ use crate::rendering::textures::fallback_tex::FallbackTextures;
 use crate::rendering::ubo::camera::CameraUBO;
 use crate::rendering::ubo::CAMERA_UBO_BINDING;
 use dawn_graphics::gl::material::Material;
+use dawn_graphics::gl::mesh::{SubMesh, TopologyBucket};
 use dawn_graphics::gl::raii::framebuffer::Framebuffer;
 use dawn_graphics::gl::raii::shader_program::Program;
 use dawn_graphics::gl::raii::texture::Texture2D;
@@ -15,6 +16,7 @@ use dawn_graphics::passes::result::RenderResult;
 use dawn_graphics::passes::RenderPass;
 use dawn_graphics::renderable::Renderable;
 use dawn_graphics::renderer::{DataStreamFrame, RendererBackend};
+use glam::Mat4;
 use glow::HasContext;
 use std::rc::Rc;
 use std::sync::Arc;
@@ -57,6 +59,61 @@ impl ForwardPass {
             gbuffer,
             camera_ubo,
         }
+    }
+
+    fn prepare_bucket(&self, bucket: &TopologyBucket) -> (bool, RenderResult) {
+        #[cfg(feature = "devtools")]
+        let tangents = if self.config.get_force_no_tangents() {
+            false
+        } else {
+            bucket.key.tangent_valid
+        };
+        #[cfg(not(feature = "devtools"))]
+        let tangents = bucket.key.tangent_valid;
+
+        let shader = self.shader.as_ref().unwrap();
+        let program = shader.asset.cast();
+        program.set_uniform(&shader.tangent_valid, tangents);
+        (false, RenderResult::default())
+    }
+
+    fn prepare_submesh(&self, model: &Mat4, submesh: &SubMesh) -> (bool, RenderResult) {
+        // Check if the submesh at the camera frustum
+        // otherwise, skip rendering
+        // TODO: Is it worth to do frustum culling per submesh?
+        if !self.frustum.is_visible(submesh.min, submesh.max, *model) {
+            return (true, RenderResult::default());
+        }
+
+        let (albedo, normal, metallic_roughness, occlusion) =
+            if let Some(material) = &submesh.material {
+                let material = material.cast::<Material>();
+
+                let albedo = material.albedo.cast();
+                let normal = material.normal.cast();
+                let metallic_roughness = material.metallic_roughness.cast();
+                let occlusion = material.occlusion.cast();
+
+                (albedo, normal, metallic_roughness, occlusion)
+            } else {
+                (
+                    &self.fallback_textures.albedo_texture,
+                    &self.fallback_textures.normal_texture,
+                    &self.fallback_textures.metallic_roughness_texture,
+                    &self.fallback_textures.occlusion_texture,
+                )
+            };
+
+        Texture2D::bind(&self.gl, albedo, ALBEDO_INDEX as u32);
+        Texture2D::bind(&self.gl, normal, NORMAL_INDEX as u32);
+        Texture2D::bind(
+            &self.gl,
+            metallic_roughness,
+            METALLIC_ROUGHNESS_INDEX as u32,
+        );
+        Texture2D::bind(&self.gl, occlusion, OCCLUSION_INDEX as u32);
+
+        (false, RenderResult::default())
     }
 }
 
@@ -160,81 +217,30 @@ impl RenderPass<RenderingEvent> for ForwardPass {
         _: &mut RendererBackend<RenderingEvent>,
         renderable: &Renderable,
     ) -> RenderResult {
-        if let Some(shader) = self.shader.as_mut() {
-            let mesh = renderable.mesh.cast();
-
-            // Check if the mesh is within the camera frustum
-            // otherwise, skip rendering it at all
-            if !self
-                .frustum
-                .is_visible(mesh.min, mesh.max, renderable.model)
-            {
-                return RenderResult::default();
-            }
-
-            // Load view matrix into shader
-            let program = shader.asset.cast();
-            program.set_uniform(&shader.model_location, renderable.model);
-
-            mesh.draw(
-                |bucket| {
-                    #[cfg(feature = "devtools")]
-                    let tangents = if self.config.get_force_no_tangents() {
-                        false
-                    } else {
-                        bucket.key.tangent_valid
-                    };
-                    #[cfg(not(feature = "devtools"))]
-                    let tangents = bucket.key.tangent_valid;
-
-                    program.set_uniform(&shader.tangent_valid, tangents);
-                    (false, RenderResult::default())
-                },
-                |submesh| {
-                    // Check if the submesh at the camera frustum
-                    // otherwise, skip rendering
-                    // TODO: Is it worth to do frustum culling per submesh?
-                    if !self
-                        .frustum
-                        .is_visible(submesh.min, submesh.max, renderable.model)
-                    {
-                        return (true, RenderResult::default());
-                    }
-
-                    let (albedo, normal, metallic_roughness, occlusion) =
-                        if let Some(material) = &submesh.material {
-                            let material = material.cast::<Material>();
-
-                            let albedo = material.albedo.cast();
-                            let normal = material.normal.cast();
-                            let metallic_roughness = material.metallic_roughness.cast();
-                            let occlusion = material.occlusion.cast();
-
-                            (albedo, normal, metallic_roughness, occlusion)
-                        } else {
-                            (
-                                &self.fallback_textures.albedo_texture,
-                                &self.fallback_textures.normal_texture,
-                                &self.fallback_textures.metallic_roughness_texture,
-                                &self.fallback_textures.occlusion_texture,
-                            )
-                        };
-
-                    Texture2D::bind(&self.gl, albedo, ALBEDO_INDEX as u32);
-                    Texture2D::bind(&self.gl, normal, NORMAL_INDEX as u32);
-                    Texture2D::bind(
-                        &self.gl,
-                        metallic_roughness,
-                        METALLIC_ROUGHNESS_INDEX as u32,
-                    );
-                    Texture2D::bind(&self.gl, occlusion, OCCLUSION_INDEX as u32);
-
-                    (false, RenderResult::default())
-                },
-            )
-        } else {
-            RenderResult::default()
+        if self.shader.is_none() {
+            return RenderResult::default();
         }
+
+        let mesh = renderable.mesh.cast();
+
+        // Check if the mesh is within the camera frustum
+        // otherwise, skip rendering it at all
+        if !self
+            .frustum
+            .is_visible(mesh.min, mesh.max, renderable.model)
+        {
+            return RenderResult::default();
+        }
+
+        let shader = self.shader.as_mut().unwrap();
+        let program = shader.asset.cast();
+
+        program.set_uniform(&shader.model_location, renderable.model);
+
+        mesh.draw(
+            |bucket| self.prepare_bucket(bucket),
+            |submesh| self.prepare_submesh(&renderable.model, submesh),
+        )
     }
 
     #[inline(always)]
